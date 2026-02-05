af997f0fc54f37cae45a75b2e858af3218c601f2
avinash-ganta0 <avinash.ganta6@gmail.com>
2026-02-05 12:51:43 +0530

    task manager

--- task-manager/backend/server.js
+++ task-manager/backend/server.js
@@ -11,9 +11,9 @@ app.use(express.static('../frontend'));
 
 // Database connection pool
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
+    host: process.env.DB_HOST || 'avinash.cmdigqa4cp2m.us-east-1.rds.amazonaws.com',
+    user: process.env.DB_USER || 'admin',
+    password: process.env.DB_PASSWORD || 'admin123',
     database: process.env.DB_NAME || 'task_manager',
     waitForConnections: true,
     connectionLimit: 10,
